2d4660b90ee43423c638214e29b15ef14c9d4219
Ali <alistair.m.obrien@gmail.com>
2026-03-21 08:44:26 +0900

    Revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import vercel from "@astrojs/vercel";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   site: 'https://alistair-obrien.github.io',
@@ -19,5 +20,11 @@ export default defineConfig({
     },
   },
 
-  output: "static",
+  output: "server",
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
